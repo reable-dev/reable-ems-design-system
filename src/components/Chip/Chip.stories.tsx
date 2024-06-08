85c846f1ab08b4cbcc39611514b4b5cafe5ea06a
Jaeha Lee <wogkdkrm112@gmail.com>
2024-06-08 14:43:24 +0900

    feat: chip storybook

---
+++ src/components/Chip/Chip.stories.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import styled from 'styled-components';
+import theme from '../../styles/theme.styles';
+
+import Chip from './Chip';
+import ChipList, { OptionType } from './ChipList';
+
+import Warning from '../Icons/Warning';
+import CloseRound from '../Icons/CloseRound';
+import WarningSVG from '../Icons/Warning';
+
+const meta: Meta<typeof Chip> = {
+  title: 'common/Chip',
+  component: Chip,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: {},
+  argTypes: {},
+};
+
+export default meta;
+
+const IconMap: any = {
+  Warning: <Warning width={16} height={16} color={theme.color.gray100} />,
+  CloseRound: <CloseRound width={16} height={16} color={theme.color.gray100} />,
+  none: undefined,
+};
+
+export const Default = (args: any) => {
+  return (
+    <ChipContainer>
+      <Chip prefixTitle="design system" {...args} />
+    </ChipContainer>
+  );
+};
+
+export const ChipListStory = (args: any) => {
+  const [selectedOptions, setSelectedOptions] = useState<OptionType<string>[]>([
+    { category: 'Category1', label: 'Option 1', value: '1' },
+    { category: 'Category2', label: 'Option 2', value: '2' },
+    { category: 'Category3', label: 'Option 3', value: '3' },
+  ]);
+
+  const handleDelete = (option: OptionType<string>) => {
+    setSelectedOptions(selectedOptions.filter((item) => item.value !== option.value));
+  };
+
+  return <ChipList selectedOptions={selectedOptions} onDelete={handleDelete} {...args} />;
+};
+
+type Story = StoryObj<typeof Chip>;
+
+const ChipContainer = styled.div`
+  border: 1px solid ${theme.color.gray200};
+  border-radius: 8px;
+  padding: 4px 8px;
+`;
